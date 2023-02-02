3863f84ae063e2eec5fff988b11784723f7094a8
vale-cala07 <123111629+vale-cala07@users.noreply.github.com>
2023-02-02 11:06:38 -0300

    Create vinculando.js

---
+++ vinculando.js
@@ -0,0 +1,7 @@
+document.getElementById("boton_color").addEventListener("click",function(){
+    document.body.style.backgroundColor= "grey";
+    });
+    
+    document.getElementById("boton_default").addEventListener("click",function(){
+        document.body.style.backgroundColor="#F5F5DC"
+    });
